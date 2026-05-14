e42a1106f6a76c862102c78753222c0bde145278
An-dr3a <criolloandrea20@hotmail.com>
2026-05-14 09:02:05 -0500

    Solucion con IA

--- src/main/java/ec/edu/utpl/computacion/proava/App.java
+++ src/main/java/ec/edu/utpl/computacion/proava/App.java
@@ -11,19 +11,32 @@ public class App {
                 {"Java", "Rust", "Java"}
         };
 
-        String palabraBuscada = "Python";
+        String palabraBuscada = "Java";
+        System.out.println("Iniciando busqueda concurrente del termino: \"" + palabraBuscada + "\"");
         int filas = matriz.length;
+
         Hilos[]  hilos = new Hilos[filas];
+        Thread[] threads = new Thread[filas];
         int resultadoFinal = 0;
 
         try {
             for(int i = 0; i < filas; i++){
-                hilos[i] = new Hilos(matriz[i], palabraBuscada, i);
+                hilos[i] = new Hilos(matriz[i], palabraBuscada);
+                threads[i] = new Thread(hilos[i]);
+                threads[i].start();
             }
 
             for(int i = 0; i < filas; i++){
-                int parcial = hilos[i].getContador();
+                threads[i].join();
+                int num = hilos[i].getContador();
+                System.out.println("Hilo -" + i + " finalizado. Encontrados: " + num);
+                resultadoFinal += hilos[i].getContador();
             }
-        }catch (Exception e){}
+
+            System.out.println("Total de coincidencias para '" + palabraBuscada + "': " + resultadoFinal);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
